c77006bad478dfd33cd63ef2a8cccc74f9fa16eb
Marian C Moldovan <moldovan0991@gmail.com>
2014-12-07 02:03:56 +0100

    pintar symbolos con el l8Signals.py

--- l8Signals.py
+++ l8Signals.py
@@ -1,98 +1,123 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
 import time
 import requests
 import l8
 import json
 
-GPIO.setmode(GPIO.BOARD)
-
-#leds pins
-
-led_off=26
-led_on=8
-
-#buttons pins
-#butt=12
-button0=3
-button1=5
-#button2=7
-#button3=13
-#button4=15 
 
 #vars
-en_juego=True;
-color0=l8.Colour(0,0,255)
-color1=l8.Colour(255,51,0)
+color_verde=l8.Colour(0,255,0)
+color_rojo=l8.Colour(255,0,0)
+color_blanco=l8.Colour(255,255,255)
 
+
+#l8 device
 l = l8.L8Bt("00:17:EC:4C:62:EE")
 
-list_mov=[]
-def encender_luz(led):
-	GPIO.setup(led, GPIO.OUT)
-	GPIO.output(led,1)
+#symbol
+list_d=[]
 
-def apagar_luz(led):
-	GPIO.setup(led, GPIO.OUT)
-	GPIO.output(led,0)
+list_d.append((0,3))
+list_d.append((2,3))
+list_d.append((4,3))
+list_d.append((1,3))
+list_d.append((3,3))
+list_d.append((5,3))
+list_d.append((7,3))
+list_d.append((0,4))
+list_d.append((2,4))
+list_d.append((4,4))
+list_d.append((1,4))
+list_d.append((3,4))
+list_d.append((5,4))
+list_d.append((7,4))
 
-def init_buttons():
-	GPIO.setup(button0, GPIO.IN)
-	GPIO.setup(button1, GPIO.IN)
 
-def init_juego():
-	time.sleep(1)
-	init_buttons()
-	apagar_luz(led_off)
-	encender_luz(led_on)
+
+
+list_ok=[]
+
+list_ok.append((3,3))
+list_ok.append((3,4))
+list_ok.append((4,2))
+list_ok.append((4,3))
+list_ok.append((2,0))
+list_ok.append((3,0))
+list_ok.append((3,1))
+list_ok.append((4,1))
+list_ok.append((0,6))
+list_ok.append((0,7))
+list_ok.append((1,5))
+list_ok.append((1,6))
+list_ok.append((2,4))
+list_ok.append((2,5))
+list_ok.append((4,2))
+list_ok.append((5,1))
+list_ok.append((5,2))
+
+
+
+list_x=[]
+
+list_x.append((0,0))
+list_x.append((1,0))
+list_x.append((1,1))
+list_x.append((2,1))
+list_x.append((2,2))
+list_x.append((3,2))
+list_x.append((3,3))
+list_x.append((3,4))
+list_x.append((4,3))
+list_x.append((4,4))
+list_x.append((5,4))
+list_x.append((5,5))
+list_x.append((6,5))
+list_x.append((6,6))
+list_x.append((7,6))
+list_x.append((7,7))
+list_x.append((0,6))
+list_x.append((0,7))
+list_x.append((1,5))
+list_x.append((1,6))
+list_x.append((2,4))
+list_x.append((2,5))
+list_x.append((4,2))
+list_x.append((5,1))
+list_x.append((5,2))
+list_x.append((6,0))
+list_x.append((6,1))
+list_x.append((7,0))
+list_x.append((4,4))
+
+
+def initial():
 	l.send_clear()
-	l.back_light(l8.Colour(0,255,0))
+	l.back_light(color_verde)
+
 	print ("ENCENDER")
 	
-def apagar_juego():
-	GPIO.cleanup()
-	encender_luz(led_off)
+def finalize():
 	l.send_clear()
+	l.back_light(color_blanco)
 
-def pintar(inicio, fin, color):
-	l.set_light(inicio, fin, color)
+def paint_x():
+	l.send_clear()
+	l.back_light(color_rojo)
+	for i in list_x:		
+		l.set_light(i[0],i[1],color_rojo)
+	
+def paint_ok():
+	l.send_clear()
+	l.back_light(color_verde)
+	for i in list_ok:		
+		l.set_light(i[0],i[1],color_verde)
 
-def movimiento(inicio , final , boton, color):
-	print("amos a probar:", inicio, final, boton)
-	pintar(inicio,final,color)
-	while GPIO.input(button0) and GPIO.input(button1) :
-                pass
-        if (GPIO.input(boton) == False) :
-                list_mov.append(True)
-                l.send_clear()
-		l.back_light(l8.Colour(0,0,0))
-		time.sleep(1)
-		l.back_light(l8.Colour(0,255,0))
-	        print ("OK")
-	else:
-		list_mov.append(False)
-		print("NOT OK")
-		l.back_light(l8.Colour(255,0,0))
-                time.sleep(1)
-                l.back_light(l8.Colour(0,255,0))
-
-	apagar_juego()
-	init_juego()
-
-
-def jugar():
-	movimiento(2,3,button1,color1)
-	movimiento(6,2,button0,color0)
-	movimiento(6,7,button1,color1)
-	movimiento(0,3,button1,color1)
-	movimiento(5,5,button0,color0)
-	movimiento(7,0,button0,color0)
-	movimiento(3,0,button1,color1)
-	movimiento(3,4,button1,color1)
-	movimiento(1,7,button0,color0)
-	movimiento(0,4,button1,color1)
-	print list_mov
+def paint_d():
+	l.send_clear()
+	l.back_light(color_rojo)
+	for i in list_d:		
+		l.set_light(i[0],i[1],color_rojo)
 
 def post_result():
 	data={"user":2,"gameType":"Basic","att1":list_mov[0],"att1_time":0.8,"att2":list_mov[1],"att2_time":0.3,"att3":list_mov[2],"att3_time":1.8,"att4":list_mov[3],"att4_time":0.5,"att5":list_mov[4],"att5_time":2.8,"att6":list_mov[5],"att6_time":0.2,"att7":list_mov[6],"att7_time":0.9,"att8":list_mov[7],"att8_time":3.8,"att9":list_mov[8],"att9_time":0.8,"att10":list_mov[9],"att10_time":0.7}
@@ -101,26 +126,17 @@ def post_result():
 	h={'Content-type': 'application/json', 'Accept': 'application/json'}
 	r=requests.post(url, data=json.dumps(data), headers=h)
 	print r
-encender_luz(led_off)
-
-init_juego()
-
-jugar()
-post_result()
-apagar_juego()
 
 
-#testing:
+#initial()
+#paint_x()
+#time.sleep(2)
+
+#paint_ok()
+#time.sleep(2)
+
+#paint_d()
+#time.sleep(2)
 
 
-
-#encender_luz(led_on)
-#time.sleep(5)
-#apagar_luz(led_on)
-
-
-
-#limpieza final :)
-
-
-
+#finalize()
