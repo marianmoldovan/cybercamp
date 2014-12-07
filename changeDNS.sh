a58c6e728ed5b848dc74ab6b6fcd396c7f907f0c
Marian C Moldovan <moldovan0991@gmail.com>
2014-12-07 02:50:26 +0100

    changed changeDNS.sh

--- changeDNS.sh
+++ changeDNS.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
 function usage(){
-echo "<script name> [o|f]"
-echo	"-o  starts the openDNS Server"
-echo    "-f  stops the openDNS server"		
+	echo "<script name> [o|f]"
+		echo	"-o  starts the openDNS Server"
+		echo    "-f  stops the openDNS server"		
 }
 
 function startOpenDNS(){
-echo "Cambiando a OpenDNS"
-sudo mv /etc/dhcp/dhcpd-opendns.conf /etc/dhcp/dhcpd.conf
-sudo service isc-dhcp-server restart
-sudo service hostapd restart
-echo "Cambio a OpenDNS realizado"
+	echo "Cambiando a OpenDNS"
+		sudo cp /etc/dhcp/dhcpd-opendns.conf /etc/dhcp/dhcpd.conf
+		sudo ifconfig wlan0 192.168.42.1
+		sudo service isc-dhcp-server restart
+		sudo service hostapd restart
+		echo "Cambio a OpenDNS realizado"
 }
 
 function stopOpenDNS(){
-echo "Quitando el OpenDNS"
-sudo mv /etc/dhcp/dhcpd-normal.conf /etc/dhcp/dhcpd.conf
-sudo service isc-dhcp-server restart
-sudo service hostapd restart
-echo "OpenDNS apagado"
+	echo "Quitando el OpenDNS"
+		sudo cp /etc/dhcp/dhcpd-normal.conf /etc/dhcp/dhcpd.conf
+		sudo ifconfig wlan0 192.168.42.1 
+		sudo service isc-dhcp-server restart
+		sudo service hostapd restart
+		echo "OpenDNS apagado"
 }
 
 while getopts ":of" opts; do
-    case "${opts}" in
-        o)
-            startOpenDNS
-            ;;
-        f)
-            stopOpenDNS
-	    ;;
-        *)
-            usage
-		exit -1
-            ;;
-    esac
+case "${opts}" in
+o)
+startOpenDNS
+;;
+f)
+stopOpenDNS
+;;
+*)
+usage
+exit -1
+;;
+esac
 done
 
 
