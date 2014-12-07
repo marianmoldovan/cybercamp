b200bc27bab4f5e3ea26e9e07be24a6114dce457
Marian C Moldovan <moldovan0991@gmail.com>
2014-12-07 04:12:13 +0100

    L8 class, connecter with L8 Smartlight done

--- l8Signals.py
+++ l8Signals.py
@@ -5,15 +5,10 @@ import requests
 import l8
 import json
 
-
 #vars
 color_verde=l8.Colour(0,255,0)
 color_rojo=l8.Colour(255,0,0)
 color_blanco=l8.Colour(255,255,255)
-
-
-#l8 device
-l = l8.L8Bt("00:17:EC:4C:62:EE")
 
 #symbol
 list_d=[]
@@ -91,52 +86,44 @@ list_x.append((7,0))
 list_x.append((4,4))
 
 
-def initial():
-	l.send_clear()
-	l.back_light(color_verde)
-
-	print ("ENCENDER")
-	
-def finalize():
-	l.send_clear()
-	l.back_light(color_blanco)
-
-def paint_x():
-	l.send_clear()
-	l.back_light(color_rojo)
-	for i in list_x:		
-		l.set_light(i[0],i[1],color_rojo)
-	
-def paint_ok():
-	l.send_clear()
-	l.back_light(color_verde)
-	for i in list_ok:		
-		l.set_light(i[0],i[1],color_verde)
-
-def paint_d():
-	l.send_clear()
-	l.back_light(color_rojo)
-	for i in list_d:		
-		l.set_light(i[0],i[1],color_rojo)
-
-def post_result():
-	data={"user":2,"gameType":"Basic","att1":list_mov[0],"att1_time":0.8,"att2":list_mov[1],"att2_time":0.3,"att3":list_mov[2],"att3_time":1.8,"att4":list_mov[3],"att4_time":0.5,"att5":list_mov[4],"att5_time":2.8,"att6":list_mov[5],"att6_time":0.2,"att7":list_mov[6],"att7_time":0.9,"att8":list_mov[7],"att8_time":3.8,"att9":list_mov[8],"att9_time":0.8,"att10":list_mov[9],"att10_time":0.7}
-	print data
-	url="http://192.168.1.12:8000/api/game/"
-	h={'Content-type': 'application/json', 'Accept': 'application/json'}
-	r=requests.post(url, data=json.dumps(data), headers=h)
-	print r
+class ELE8:
+	def __init__(self):
+		#l8 device
+		self.l = l8.L8Bt("00:17:EC:4C:62:EE")
+		return None
 
 
-#initial()
-#paint_x()
-#time.sleep(2)
+	def initial(self):
+		self.l.send_clear()
+		self.l.back_light(color_verde)
+		print ("ENCENDER")
+		
+	def finalize(self):
+		self.l.send_clear()
+		self.l.back_light(color_blanco)
 
-#paint_ok()
-#time.sleep(2)
+	def paint_x(self):
+		self.l.send_clear()
+		self.l.back_light(color_rojo)
+		for i in list_x:		
+			self.l.set_light(i[0],i[1],color_rojo)
+		
+	def paint_ok(self):
+		self.l.send_clear()
+		self.l.back_light(color_verde)
+		for i in list_ok:		
+			self.l.set_light(i[0],i[1],color_verde)
 
-#paint_d()
-#time.sleep(2)
+	def paint_d(self):
+		self.l.send_clear()
+		self.l.back_light(color_rojo)
+		for i in list_d:		
+			self.l.set_light(i[0],i[1],color_rojo)
 
-
-#finalize()
+	def post_result(self):
+		data={"user":2,"gameType":"Basic","att1":list_mov[0],"att1_time":0.8,"att2":list_mov[1],"att2_time":0.3,"att3":list_mov[2],"att3_time":1.8,"att4":list_mov[3],"att4_time":0.5,"att5":list_mov[4],"att5_time":2.8,"att6":list_mov[5],"att6_time":0.2,"att7":list_mov[6],"att7_time":0.9,"att8":list_mov[7],"att8_time":3.8,"att9":list_mov[8],"att9_time":0.8,"att10":list_mov[9],"att10_time":0.7}
+		print data
+		url="http://192.168.1.12:8000/api/game/"
+		h={'Content-type': 'application/json', 'Accept': 'application/json'}
+		r=requests.post(url, data=json.dumps(data), headers=h)
+		print r
